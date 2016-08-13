67df404d6bca5538599877d56e610592e822c8ea
GitZwj <zwj1206_hi@163.com>
2016-08-13 11:07:46 +0800

    bug

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1470997311,
-    'last_time' => 1470993711,
+    'due_time' => 1471058318,
+    'last_time' => 1471054718,
   ),
 );
 
