9d870386172f6281326745ba1921e152380b9778
GitZwj <zwj1206_hi@163.com>
2016-08-19 14:25:26 +0800

    安装SQL修改

--- external/modules/rapp/lib/methods.lib.php
+++ external/modules/rapp/lib/methods.lib.php
@@ -1,32 +1,37 @@
 <?php
 
+namespace rapp\lib;
+
 /**
  * 目录封装类
  * @author Mosquito
  * @link www.360cd.cn
  */
-class Dir {
+class Dir
+{
 
     /**
      * 初始化一个实例
-     * 
+     *
      * @return Dir
      */
-    static function init() {
+    public static function init()
+    {
         return new Dir();
     }
 
     /**
      * 创建目录
-     * 
-     * @param string $dir            
+     *
+     * @param string $dir
      * @return string $dir
      */
-    function create_dir($path) {
-        $path = str_replace('\\', '/', $path);
+    public function create_dir($path)
+    {
+        $path       = str_replace('\\', '/', $path);
         $path_array = explode('/', $path);
-        $str = '';
-        foreach ( $path_array as $dir ) {
+        $str        = '';
+        foreach ($path_array as $dir) {
             if (!$dir) {
                 continue;
             }
@@ -41,22 +46,24 @@ class Dir {
 
     /**
      * 获取当前目录及子目录下所有文件名
-     * 
-     * @param string $path            
+     *
+     * @param string $path
      * @param
      *            array &$file_list
-     * @param string $remove_path            
+     * @param string $remove_path
      */
-    function get_dir_filename($path, $remove_path = '') {
+    public function get_dir_filename($path, $remove_path = '')
+    {
         $file_list = array();
         $this->_get_dir_filename($path, $file_list, $remove_path);
         return $file_list;
     }
 
-    protected function _get_dir_filename($path, &$file_list, $remove_path = '') {
+    protected function _get_dir_filename($path, &$file_list, $remove_path = '')
+    {
         if (is_dir($path)) {
             $dp = dir($path);
-            while ( $file = $dp->read() ) {
+            while ($file = $dp->read()) {
                 if ($file != '.' && $file != '..') {
                     $this->_get_dir_filename($path . '/' . $file, $file_list, $remove_path);
                 }
@@ -69,12 +76,13 @@ class Dir {
 
     /**
      * 删除当前目录以及目录下所有文件
-     * 
-     * @param string $dir            
+     *
+     * @param string $dir
      */
-    function del_dir($dir) {
+    public function del_dir($dir)
+    {
         $dh = opendir($dir);
-        while ( $file = readdir($dh) ) {
+        while ($file = readdir($dh)) {
             if ($file != "." && $file != "..") {
                 $fullpath = $dir . "/" . $file;
                 if (!is_dir($fullpath)) {
@@ -90,12 +98,13 @@ class Dir {
 
     /**
      * 删除指定目录及子目录下空的目录
-     * 
-     * @param string $path            
+     *
+     * @param string $path
      */
-    function del_empty_dir($path) {
+    public function del_empty_dir($path)
+    {
         if (is_dir($path) && ($handle = opendir($path)) != false) {
-            while ( ($file = readdir($handle)) != false ) {
+            while (($file = readdir($handle)) != false) {
                 if ($file != '.' && $file != '..') {
                     $curfile = $path . '/' . $file;
                     if (is_dir($curfile)) {
@@ -112,13 +121,14 @@ class Dir {
 
     /**
      * 拷贝目录文件到指定目录
-     * 
-     * @param string $source            
-     * @param string $dest            
+     *
+     * @param string $source
+     * @param string $dest
      */
-    function copy_dir($source, $dest) {
+    public function copy_dir($source, $dest)
+    {
         $dir = opendir($source);
-        while ( false != ($file = readdir($dir)) ) {
+        while (false != ($file = readdir($dir))) {
             if (($file != '.') && ($file != '..')) {
                 if (is_dir($source . '/' . $file)) {
                     $this->copy_dir($source . '/' . $file, $dest . '/' . $file);
@@ -137,75 +147,80 @@ class Dir {
 
 /**
  * 通用方法封装类
- * 
+ *
  * @author Mosquito
  * @link www.360cd.cn
  */
-class Methods {
+class Methods
+{
 
     /**
      * 根据时间戳获取当天的开始时间戳与结束时间戳
-     * 
-     * @param bigint $timestamp            
+     *
+     * @param bigint $timestamp
      * @param bool $zone
      *            是否计算时区，默认启用
      */
-    static function get_day_timestamp($timestamp, $zone = true) {
+    public static function get_day_timestamp($timestamp, $zone = true)
+    {
         $timezone = $zone ? date('Z') : 0;
-        $Y_m_d = date('Y-m-d', $timestamp + $timezone);
-        
-        $day = array();
+        $Y_m_d    = date('Y-m-d', $timestamp + $timezone);
+
+        $day   = array();
         $day[] = strtotime($Y_m_d) - $timezone;
         $day[] = $day[0] + 86400;
-        
+
         return $day;
     }
 
     /**
      * 根据时间戳获取当前周的开始时间戳与结束时间戳
-     * 
-     * @param bigint $timestamp            
+     *
+     * @param bigint $timestamp
      * @param bool $zone
      *            是否计算时区，默认启用
      */
-    static function get_week_timestamp($timestamp, $zone = true) {
+    public static function get_week_timestamp($timestamp, $zone = true)
+    {
         $timezone = $zone ? date('Z') : 0;
-        $date = getdate($timestamp + $timezone);
-        
+        $date     = getdate($timestamp + $timezone);
+
         $Y_m_d = date('Y-m-d', $timestamp + $timezone);
-        $day = strtotime($Y_m_d) - $timezone;
-        
-        $week = array();
+        $day   = strtotime($Y_m_d) - $timezone;
+
+        $week   = array();
         $week[] = $day - $date['wday'] * 86400;
         $week[] = $week[0] + 7 * 86400;
-        
+
         return $week;
     }
 
     /**
      * 保存数组配置型文件
-     * 
-     * @param string $filename            
-     * @param array $data            
+     *
+     * @param string $filename
+     * @param array $data
      * @return array
      */
-    static function save_config($filename, $data) {
+    public static function save_config($filename, $data)
+    {
         return file_put_contents($filename, "<?php\nreturn " . var_export($data, true) . ";\n?>");
     }
 
     /**
      * 加载数组配置型文件
-     * 
-     * @param string $filename            
+     *
+     * @param string $filename
      * @param string $key
      *            'a:b:c'支持多层
      * @return mixed
      */
-    static function load_config($filename, $key = '') {
-        $config = file_exists($filename) ? include ($filename) : array();
+    public static function load_config($filename, $key = '')
+    {
+        $config = file_exists($filename) ? include $filename : array();
         if ($key != '') {
             $key_arr = explode(':', $key);
-            foreach ( $key_arr as $v ) {
+            foreach ($key_arr as $v) {
                 $config = $config[$v];
             }
         }
@@ -214,23 +229,25 @@ class Methods {
 
     /**
      * 获取一个GUID字串
-     * 
-     * @param string $glue            
+     *
+     * @param string $glue
      * @return string
      */
-    static function get_guid($glue = '') {
+    public static function get_guid($glue = '')
+    {
         $charid = strtoupper(md5(uniqid(mt_rand(), true)));
         $hyphen = $glue;
-        $guid = substr($charid, 6, 2) . substr($charid, 4, 2) . substr($charid, 2, 2) . substr($charid, 0, 2) . $hyphen . substr($charid, 10, 2) . substr($charid, 8, 2) . $hyphen . substr($charid, 14, 2) . substr($charid, 12, 2) . $hyphen . substr($charid, 16, 4) . $hyphen . substr($charid, 20, 12);
+        $guid   = substr($charid, 6, 2) . substr($charid, 4, 2) . substr($charid, 2, 2) . substr($charid, 0, 2) . $hyphen . substr($charid, 10, 2) . substr($charid, 8, 2) . $hyphen . substr($charid, 14, 2) . substr($charid, 12, 2) . $hyphen . substr($charid, 16, 4) . $hyphen . substr($charid, 20, 12);
         return $guid;
     }
 
     /**
      * 获取客户端真实ip
-     * 
+     *
      * @return string
      */
-    static function get_client_ip() {
+    public static function get_client_ip()
+    {
         if (getenv('HTTP_CLIENT_IP')) {
             $ip = getenv('HTTP_CLIENT_IP');
         } else if (getenv('HTTP_X_FORWARDED_FOR')) {
@@ -247,14 +264,15 @@ class Methods {
 
     /**
      * url操作：获取、添加、删除
-     * 
-     * @param string $url            
-     * @param string $name            
-     * @param string $value            
-     * @param bool $del            
+     *
+     * @param string $url
+     * @param string $name
+     * @param string $value
+     * @param bool $del
      * @return string|mixed
      */
-    static function url($url = '', $name = '', $value = '', $del = false) {
+    public static function url($url = '', $name = '', $value = '', $del = false)
+    {
         if ($url == '') {
             $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'];
         } else if ($url == 'host') {
@@ -262,14 +280,14 @@ class Methods {
         } else if ($url == 'host|self') {
             $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
         }
-        
+
         if ($name == '') {
             return $url;
         }
-        
+
         $str = '';
         preg_match('/(\\\?|&)' . $name . '=([^&]+)(&|$)/', $url, $str);
-        
+
         if (!$del) {
             if ($str[1] == '?') {
                 return str_replace($str[0], '?', $url);
@@ -305,14 +323,15 @@ class Methods {
      * @param string $action
      * @param array $param
      */
-    static function curl($url, $action = 'GET', $param = array(), $decode = true) {
+    public static function curl($url, $action = 'GET', $param = array(), $decode = true)
+    {
         $action = strtoupper($action);
         ini_set('open_basedir', '');
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $action);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
         if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
