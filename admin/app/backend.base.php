f17b6506c597e374cec8d2059f71fa79f7e906fa
born <liubowater@zoliu.cn>
2017-04-01 11:19:51 +0800

    2. 修改了Nginx后台的兼容性 3. 修改了语言文件jslang调用的错误

--- admin/app/backend.base.php
+++ admin/app/backend.base.php
@@ -6,217 +6,190 @@
  *    @author    Garbin
  *    @usage    none
  */
-class BackendApp extends ECBaseApp
-{
-    function __construct()
-    {
-        $this->BackendApp();
-    }
-    function BackendApp()
-    {
-        Lang::load(lang_file('admin/common'));
-        Lang::load(lang_file('admin/' . APP));
-        parent::__construct();
-        
-        //---www.360cd.cn  Mosquito---
-        //引入钱包方法相关
-        import('zllib/money.lib');
-    }
-    function login()
-    {
-        if ($this->visitor->has_login)
-        {
-            $this->show_warning('has_login');
+class BackendApp extends ECBaseApp {
+	function __construct() {
+		$this->BackendApp();
+	}
+	function BackendApp() {
+		Lang::load(lang_file('admin/common'));
+		Lang::load(lang_file('admin/' . APP));
+		parent::__construct();
 
-            return;
-        }
-        if (!IS_POST)
-        {
-            if (Conf::get('captcha_status.backend'))
-            {
-                $this->assign('captcha', 1);
-            }
-            $this->display('login.html');
-        }
-        else
-        {
-            if (Conf::get('captcha_status.backend') && base64_decode($_SESSION['captcha']) != strtolower($_POST['captcha']))
-            {
-                $this->show_warning('captcha_faild');
+		//---www.360cd.cn  Mosquito---
+		//引入钱包方法相关
+		import('zllib/money.lib');
+	}
+	function login() {
+		if ($this->visitor->has_login) {
+			$this->show_warning('has_login');
 
-                return;
-            }
+			return;
+		}
+		if (!IS_POST) {
+			if (Conf::get('captcha_status.backend')) {
+				$this->assign('captcha', 1);
+			}
+			$this->display('login.html');
+		} else {
+			if (Conf::get('captcha_status.backend') && base64_decode($_SESSION['captcha']) != strtolower($_POST['captcha'])) {
+				$this->show_warning('captcha_faild');
 
-            $user_name = trim($_POST['user_name']);
-            $password  = $_POST['password'];
+				return;
+			}
 
-            // $ms =& ms();
-            // $user_id = $ms->user->auth($user_name, $password);
-            //360cd.cn
-            $admin_model=&m('admin');
-            $user_id=$admin_model->doLogin($user_name,$password);
-            if ($user_id<1)
-            {
-                /* 未通过验证，提示错误信息 */
-                // $this->show_warning($ms->user->get_error());
-                $this->show_warning($admin_model->get_error());
+			$user_name = trim($_POST['user_name']);
+			$password = $_POST['password'];
 
-                return;
-            }
-            //360cd.cn
+			// $ms =& ms();
+			// $user_id = $ms->user->auth($user_name, $password);
+			//360cd.cn
+			$admin_model = &m('admin');
+			$user_id = $admin_model->doLogin($user_name, $password);
+			if ($user_id < 1) {
+				/* 未通过验证，提示错误信息 */
+				// $this->show_warning($ms->user->get_error());
+				$this->show_warning($admin_model->get_error());
 
-            /* 通过验证，执行登陆操作 */
-            if (!$this->_do_login($user_id))
-            {
-                return;
-            }
+				return;
+			}
+			//360cd.cn
 
-            $this->show_message('login_successed',
-                'go_to_admin', 'index.php');
-        }
-    }
+			/* 通过验证，执行登陆操作 */
+			if (!$this->_do_login($user_id)) {
+				return;
+			}
 
-    function logout()
-    {
-        parent::logout();
-        $this->show_message('logout_successed',
-            'go_to_admin', 'index.php');
-    }
+			$this->show_message('login_successed',
+				'go_to_admin', 'index.php');
+		}
+	}
 
-    /**
-     * 执行登陆操作
-     *
-     * @param int $user_id
-     * @return bool
-     */
-    function _do_login($user_id)
-    {
-        $mod_user =& m('member');
-        $user_info = $mod_user->get(array(
-            'conditions' => $user_id,
-            'join'       => 'manage_mall',
-            'fields'     => 'this.user_id, user_name, reg_time, last_login, last_ip, user_priv.privs'
-        ));
+	function logout() {
+		parent::logout();
+		$this->show_message('logout_successed',
+			'go_to_admin', 'index.php');
+	}
 
-        if (!$user_info['privs'])
-        {
-            $this->show_warning('not_admin');
+	/**
+	 * 执行登陆操作
+	 *
+	 * @param int $user_id
+	 * @return bool
+	 */
+	function _do_login($user_id) {
+		$mod_user = &m('member');
+		$user_info = $mod_user->get(array(
+			'conditions' => $user_id,
+			'join' => 'manage_mall',
+			'fields' => 'this.user_id, user_name, reg_time, last_login, last_ip, user_priv.privs',
+		));
 
-            return false;
-        }
+		if (!$user_info['privs']) {
+			$this->show_warning('not_admin');
 
-        /* 分派身份 */
-        $this->visitor->assign(array(
-            'user_id'       => $user_info['user_id'],
-            'user_name'     => $user_info['user_name'],
-            'reg_time'      => $user_info['reg_time'],
-            'last_login'    => $user_info['last_login'],
-            'last_ip'       => $user_info['last_ip'],
-        ));
+			return false;
+		}
 
-        /* 更新登录信息 */
-        $time = gmtime();
-        $ip   = real_ip();
-        $mod_user->edit($user_id, "last_login = '{$time}', last_ip='{$ip}', logins = logins + 1");
+		/* 分派身份 */
+		$this->visitor->assign(array(
+			'user_id' => $user_info['user_id'],
+			'user_name' => $user_info['user_name'],
+			'reg_time' => $user_info['reg_time'],
+			'last_login' => $user_info['last_login'],
+			'last_ip' => $user_info['last_ip'],
+		));
 
-        return true;
-    }
+		/* 更新登录信息 */
+		$time = gmtime();
+		$ip = real_ip();
+		$mod_user->edit($user_id, "last_login = '{$time}', last_ip='{$ip}', logins = logins + 1");
 
-    /**
-     *    获取JS语言项
-     *
-     *    @author    Garbin
-     *    @param    none
-     *    @return    void
-     */
-    function jslang($lang = '')
-    {
-        $lang = Lang::fetch(lang_file('admin/jslang'));
-        parent::jslang($lang);
-    }
+		return true;
+	}
 
-    /**
-     *    后台的需要权限验证机制
-     *
-     *    @author    Garbin
-     *    @return    void
-     */
-    function _run_action()
-    {
-        /* 先判断是否登录 */
-        if (!$this->visitor->has_login)
-        {
-            $this->login();
+	/**
+	 *    获取JS语言项
+	 *
+	 *    @author    Garbin
+	 *    @param    none
+	 *    @return    void
+	 */
+	function jslang($lang = '') {
+		$lang = Lang::fetch(lang_file('admin/jslang'));
+		parent::jslang($lang);
+	}
 
-            return;
-        }
+	/**
+	 *    后台的需要权限验证机制
+	 *
+	 *    @author    Garbin
+	 *    @return    void
+	 */
+	function _run_action() {
+		/* 先判断是否登录 */
+		if (!$this->visitor->has_login) {
+			$this->login();
 
-        /* 登录后判断是否有权限 */
-        if (!$this->visitor->i_can('do_action', $this->visitor->get('privs')))
-        {
-            $this->show_warning('no_permission');
+			return;
+		}
 
-            return;
-        }
+		/* 登录后判断是否有权限 */
+		if (!$this->visitor->i_can('do_action', $this->visitor->get('privs'))) {
+			$this->show_warning('no_permission');
 
-        /* 运行 */
-        parent::_run_action();
-    }
+			return;
+		}
 
-    function _config_view()
-    {
-        parent::_config_view();
-        $this->_view->template_dir  = APP_ROOT . '/templates';
-        $this->_view->compile_dir   = ROOT_PATH . '/temp/compiled/admin';
-        $this->_view->res_base      = site_url() . '/templates';
-        $this->_view->lib_base      = dirname(site_url()) . '/includes/libraries/javascript';
-    }
-    
-    /**
-     *   获取商城当前模板名称
-     */
-    function _get_template_name()
-    {
-        $template_name = Conf::get('template_name');
-        if (!$template_name)
-        {
-            $template_name = 'default';
-        }
+		/* 运行 */
+		parent::_run_action();
+	}
 
-        return $template_name;
-    }
+	function _config_view() {
+		parent::_config_view();
+		$this->_view->template_dir = APP_ROOT . '/templates';
+		$this->_view->compile_dir = ROOT_PATH . '/temp/compiled/admin';
+		$this->_view->res_base = BASE_URL . '/templates';
+		$this->_view->lib_base = dirname(site_url()) . '/includes/libraries/javascript';
+	}
 
-    /**
-     *    获取商城当前风格名称
-     */
-    function _get_style_name()
-    {
-        $style_name = Conf::get('style_name');
-        if (!$style_name)
-        {
-            $style_name = 'default';
-        }
+	/**
+	 *   获取商城当前模板名称
+	 */
+	function _get_template_name() {
+		$template_name = Conf::get('template_name');
+		if (!$template_name) {
+			$template_name = 'default';
+		}
 
-        return $style_name;
-    }
-    
-    function _init_visitor()
-    {
-        $this->visitor =& env('visitor', new AdminVisitor());
-    }
+		return $template_name;
+	}
 
-    /* 清除缓存 */
-    function _clear_cache()
-    {
-        $cache_server =& cache_server();
-        $cache_server->clear();
-    }
-    
-    function display($tpl)
-    {
-        $this->assign('real_backend_url', site_url());
-        parent::display($tpl);
-    }
+	/**
+	 *    获取商城当前风格名称
+	 */
+	function _get_style_name() {
+		$style_name = Conf::get('style_name');
+		if (!$style_name) {
+			$style_name = 'default';
+		}
+
+		return $style_name;
+	}
+
+	function _init_visitor() {
+		$this->visitor = &env('visitor', new AdminVisitor());
+	}
+
+	/* 清除缓存 */
+	function _clear_cache() {
+		$cache_server = &cache_server();
+		$cache_server->clear();
+	}
+
+	function display($tpl) {
+		$this->assign('real_backend_url', site_url());
+		parent::display($tpl);
+	}
 }
 
 /**
@@ -225,36 +198,34 @@ class BackendApp extends ECBaseApp
  *    @author    Garbin
  *    @usage    none
  */
-class AdminVisitor extends BaseVisitor
-{
-    var $_info_key = 'admin_info';
-    /**
-     *    获取用户详细信息
-     *
-     *    @author    Garbin
-     *    @param    none
-     *    @return    void
-     */
-    function _get_detail()
-    {
-        $model_member =& m('member');
-        $detail = $model_member->get(array(
-            'conditions'    => "member.user_id = '{$this->info['user_id']}'",
-            'join'          => 'manage_mall',                 //关联查找看看是否有店铺
-        ));
-        unset($detail['user_id'], $detail['user_name'], $detail['reg_time'], $detail['last_login'], $detail['last_ip']);
+class AdminVisitor extends BaseVisitor {
+	var $_info_key = 'admin_info';
+	/**
+	 *    获取用户详细信息
+	 *
+	 *    @author    Garbin
+	 *    @param    none
+	 *    @return    void
+	 */
+	function _get_detail() {
+		$model_member = &m('member');
+		$detail = $model_member->get(array(
+			'conditions' => "member.user_id = '{$this->info['user_id']}'",
+			'join' => 'manage_mall', //关联查找看看是否有店铺
+		));
+		unset($detail['user_id'], $detail['user_name'], $detail['reg_time'], $detail['last_login'], $detail['last_ip']);
 
-        return $detail;
-    }
+		return $detail;
+	}
 }
 
 /* 实现消息基础类接口 */
 class MessageBase extends BackendApp {};
 
 /* 实现模块基础类接口 */
-class BaseModule  extends BackendApp {};
+class BaseModule extends BackendApp {};
 
 /* 消息处理器 */
-require(ROOT_PATH . '/eccore/controller/message.base.php');
+require ROOT_PATH . '/eccore/controller/message.base.php';
 
 ?>
--- data/upgrade.inc.php
+++ data/upgrade.inc.php
@@ -1,11 +1,10 @@
-<?php	
- return array (
+<?php	 return array (
   'appid' => '',
   'appkey' => '',
   'system' => 'ecmos',
   'version' => '0.00.009',
-  'next_version' => '0.00.009',
-  'upgrade_time' => 1474036691,
-  'remark' => 'PHA+MS4g6aqM6K+B56CB5LiN6IO95pi+56S655qEQlVH77yM55Sx5LqO5a696auY5pWw5YC855qE6ZSZ6K+v5byV6LW355qE44CCPC9wPg0KPHA+Mi4g5L+u5pS55LqG5Y+M5qC45rWP6KeI5Zmo55qE6Leo5YaF5qC455m75b2V5YW85a655Yqf6IO944CCPC9wPg0KPHA+My4g5L+u5pS55LqG5ZCO5Y+w5Zyo5L2O54mI5pysSUXnmoTkuI3lhbzlrrnpl67popjvvIzljrvmjonkuobpg6jliIZJQ09O77yM5pS55oiQ5paH5a2X77yM5Lul5L6/5YW85a655L2O54mI5pysSUU8L3A+DQo8cD40LiDkv67mlLnkuobpppbpobXlnKjkvY7niYjmnKxJReeahOWFvOWuuemXrumimOOAgjwvcD4NCjxwPjUuIOWinuWKoOazqOWGjOWFs+mUruivjei/h+a7pOaPkuS7tu+8jOWPr+S7pei/h+a7pOazqOWGjOeahOWFs+mUruivje+8jOS4jeWFgeiuuOafkOS6m+ivjeivreiiq+azqOWGjOOAgumcgOimgeWcqOWQjuWPsC/mianlsZUv5o+S5Lu25aSE5byA5ZCvPC9wPg0KPHA+Ni4g5aKe5Yqg5LqG5Yy/5ZCN55m76ZmG5Yqf6IO9PC9wPg0KPHA+Ny4g5L+u5aSN5LqG5omL5py654mI55m76ZmG5pe2Z3JhZGXooajplJnor688L3A+',
-);	
-?>
+  'next_version' => '0.00.018',
+  'upgrade_time' => 1490601336,
+  'remark' => 'PHA+IyMjIDIwMTctMDMtMjUg5pu05paw5pel5b+XPC9wPg0KPHA+MS4g5aKe5YqgaW5fZ29vZHNfaWTlnKhlY21fZ29vZHPooajnlKjkuo7lupPlrZg8L3A+DQo8cD4yLiDkv67mlLnkuobooajlr7zlh7rnmoTlvJXmjJrlh7rplJlUWVBFPU1ZSVNBTemXrumimCZuYnNwOzwvcD4NCjxwPjMuIOS/ruaUueS6huWQjuWPsOe8lui+keaooeadv+aXtueahOiwg+eUqOWHuumUmemXrumimCZuYnNwOzwvcD4NCjxwPjQuIOS/ruWkjeS6huW+ruS/oeiwg+eUqOeahOi/nOeoi+i3r+W+hDwvcD4NCjxwPjUuIOWIoOmZpOS6hi9pbmNsdWRlcy9saWJyYXJpZXMvSHR0cC5saWIucGhwPC9wPg0KPHA+Ni4g5aKe5Yqg5LqG5LqR5bqT5Yqf6IO977yM5bmz5Y+w5Y+v5Lul5Li65oyH5a6a5bqX6ZO65YiG6YWN5oyH5a6a5ZWG5ZOBPC9wPg0KPHA+Ny4g566h55CG5ZGY5Y+v5Lul5re75Yqg5ZWG5ZOB77yM5L+u5pS55ZWG5ZOB77yM5Lul5Y+K5L+u5pS55ZWG6ZO65ZWG5ZOB55qE5aaC6KeE5qC877yM5Lu35qC85LmL57G755qEPC9wPg0KPHA+OC4g5L+u5aSN5LqGUEhQ54mI5pys5Y2H57qn55qERnJvbnRlbmRBcHDnmoTohJrmnKzplJnor6/jgII8L3A+DQo8cD45LiDkv67lpI3lkI7lj7DnrqHnkIblkZjnlYzpnaJqcXVlcnnosIPnlKjlsYLnuqflh7rplJk8L3A+DQo8cD4xMC4g5omL5Yqo5L+u5pS5U1FM77yaQUxURVIgVEFCTEUgY2hfZ29vZHMgQUREIENPTFVNTiBgaW5fZ29vZHNfaWRgIElOVCBOVUxMIDsg6Ieq5Yqo5Y2H57qnZ29vZHPooajkv67mlLnlpLHotKXml7bkvb/nlKg8L3A+DQo8ZGl2PjxiciAvPjwvZGl2Pg==',
+  'system_remark' => 'RUNNT1PlrprliLbniYjmmK8zNjBjZC5jbuagueaNruiHqui6q+WkmuW5tOS4muWKoeiuouWItueahOWPr+WFjei0ueWNh+e6p+WPimJ1Z+e7tOaKpOeahOezu+e7nyzogIHnmoTniYjmnKzlj6/og73lnKjljYfnuqfml7bmnInkuIDkuptCVUfvvIzlj6/ku6Xlj43ppojliLBRUee+pO+8mjQ3MTAxNzg377yMMTM2NDc0NTM3',
+);	?>
